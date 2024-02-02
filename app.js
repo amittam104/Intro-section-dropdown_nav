fd35dc10b2ce638b77c19e7d23e126f20980ba84
Amit Tambulkar <amittambulkar104@gmail.com>
2024-02-02 20:38:32 +0530

    chore: update nav click events

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 "use strict";
 
-const featureEl = document.querySelector(".btn--website--Features");
-const companyEl = document.querySelector(".btn--website--Company");
+const featureEl = document.querySelector(".dropdown--features");
+const companyEl = document.querySelector(".dropdown--Company");
+const dropdownFeatureEl = document.querySelector(".dropdown-feature");
+const dropdownCompanyEl = document.querySelector(".dropdown-company");
+
+featureEl.addEventListener("click", function () {
+  dropdownFeatureEl.classList.toggle("hidden");
+});
+companyEl.addEventListener("click", function () {
+  dropdownCompanyEl.classList.toggle("hidden");
+});
