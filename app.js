d4b1c92512f75b728b2574034295de3dac72acee
Amit Tambulkar <amittambulkar104@gmail.com>
2024-02-03 08:08:43 +0530

    chore: update dropdown style

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const featureEl = document.querySelector(".dropdown--features");
 const companyEl = document.querySelector(".dropdown--Company");
 const dropdownFeatureEl = document.querySelector(".dropdown-feature");
 const dropdownCompanyEl = document.querySelector(".dropdown-company");
+const openArrowFeatEl = document.querySelector(".open-arrow-feat");
+const openArrowCompEl = document.querySelector(".open-arrow-comp");
+const closeArrowFeatEl = document.querySelector(".close-arrow-feat");
+const closeArrowCompEl = document.querySelector(".close-arrow-comp");
 
 featureEl.addEventListener("click", function () {
   dropdownFeatureEl.classList.toggle("hidden");
+  openArrowFeatEl.classList.toggle("hidden");
+  closeArrowFeatEl.classList.toggle("hidden");
 });
 companyEl.addEventListener("click", function () {
   dropdownCompanyEl.classList.toggle("hidden");
+  openArrowCompEl.classList.toggle("hidden");
+  closeArrowCompEl.classList.toggle("hidden");
 });
